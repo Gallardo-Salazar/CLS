28080c1b40f2b874be19c282c005d5e16824a063
unknown <pe_gasa@hotmail.com>
2021-03-22 09:04:31 +0100

    Initial Commit

---
+++ pointclouds/Meshroom1/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3195936,
+    "boundingBox": {
+        "lx": -4.533090114593506,
+        "ly": -0.6627200245857239,
+        "lz": -2.768389940261841,
+        "ux": 14.564800262451172,
+        "uy": 18.435170352458955,
+        "uz": 16.329500436782838
+    },
+    "tightBoundingBox": {
+        "lx": -4.5330901145874,
+        "ly": -0.6627200240330899,
+        "lz": -2.7683899367522004,
+        "ux": 14.564800260071002,
+        "uy": 7.34830999198897,
+        "uz": 7.772930144424299
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.16539257764816285,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
